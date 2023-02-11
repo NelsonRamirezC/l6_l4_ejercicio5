3fc92cccbe774793c4f583bcfc04c67eebcffebd
Nelson Ramirez <xhyskoax@gmail.com>
2023-02-10 21:37:22 -0300

    avance 10-02-2023

--- index.js
+++ index.js
@@ -10,9 +10,29 @@ router.get("/", (req, res) =>{
     res.render("home");
 })
 
-router.get("/contactos", (req, res) =>{
-    res.render("contactos");
+router.get("/peliculas", (req, res) =>{
+    fs.readFile("peliculas.json", "utf8", (error, data) => {
+        if(error) return res.status(500).send({code: 500, message:"Algo salió al leer la BD."})
+        let objetoPeliculas = JSON.parse(data);
+        res.render("peliculas", {
+            peliculas: objetoPeliculas,
+            titulo: "Sección películas de NETFLIX."
+        });
+    })
 })
+
+router.get("/series", (req, res) =>{
+    fs.readFile("series.json", "utf8", (error, data) => {
+        if(error) return res.status(500).send({code: 500, message:"Algo salió al leer la BD."})
+        let objetoSeries = JSON.parse(data);
+        res.render("series", {
+            series: objetoSeries,
+            titulo: "Sección series de NETFLIX.",
+            layout: false
+        });
+    })
+})
+
 
 //endpoints
 router.get("/netflix/:opcion", (req, res) =>{
@@ -22,7 +42,7 @@ router.get("/netflix/:opcion", (req, res) =>{
         fs.readFile(opcion+".json", "utf8", (error, data) => {
             if(error) return res.status(500).send({code: 500, message:"Algo salió al leer la BD."})
             let datos = JSON.parse(data);
-            res.json(datos)
+            res.json(datos);
         })
     }else{
         res.status(400).send({code: 400, message:"Ha elegido una opción no considerada, opciones disponibles: [peliculas - series]"})
